3de833b462ba45146204a904d77d44670d339526
PLyons <pel1007@yahoo.com>
2025-05-21 23:01:29 -0400

    Initial Commit

---
+++ Iq-BibleTest/Iq_BibleTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Iq_BibleTestApp.swift
+//  Iq-BibleTest
+//
+//  Created by Paul Lyons on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Iq_BibleTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
